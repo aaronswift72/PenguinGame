6cee2957e4cde4110b94fe883a279d3ab6fe295f
Aaron Swift <aaronswift72@gmail.com>
2026-04-08 15:20:09 -0500

    downward momentum on penguin
    parameters still need tweaking

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PenguinController : MonoBehaviour
 {
     public float speed = 5f;
+    public float smoothTime = 1f;
+
+    public float diveForceMultiplier = 2.5f;
+    public float diveAcceleration = 20f;
+
     public Rigidbody2D rb;
-    private Vector2 velocityRef = Vector2.zero;
+    private float xVelocityRef = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
-        Vector2 currentVelocity = rb.linearVelocity;
-        Vector2 targetVelocity = new Vector2(speed, currentVelocity.y);
-        rb.linearVelocity = Vector2.SmoothDamp(currentVelocity, targetVelocity, ref velocityRef, 1f);
+        float smoothedX = Mathf.SmoothDamp(rb.linearVelocity.x, speed, ref xVelocityRef, smoothTime);
+        rb.linearVelocity = new Vector2(smoothedX, rb.linearVelocity.y);
+        //Vector2 currentVelocity = rb.linearVelocity;
+        //Vector2 targetVelocity = new Vector2(speed, currentVelocity.y);
+
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            float downwardBoost = Mathf.Abs(rb.linearVelocity.x) * diveForceMultiplier;
+            rb.AddForce(Vector2.down * downwardBoost * diveAcceleration * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
     }
 }
